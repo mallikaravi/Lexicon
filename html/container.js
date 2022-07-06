e0eb6acfb973e7b7f9c673a7958590ce83fe5084
mallikaravi <mailtomallikaravi@gmail.com>
2022-07-06 17:45:41 +0200

    Added  HTML file and JS file

---
+++ html/container.js
@@ -0,0 +1,48 @@
+var phrase ="Yet another Phrase";
+var segment = phrase.slice(6,11);
+<!--  
+ function addTwoNumbers(a,b){
+        var result = a+b;
+   return result;
+ }
+var x=addTwoNumbers(5,10);
+alert(x);
+addTwoNumbers(50,10);
+addTwoNumbers(500,10);
+
+var name= prompt("What is your name?");-->
+
+
+var x =45;
+while (x>2){
+    x--;
+
+}
+
+alert(x);
+
+
+
+var phrase = "This is a book";
+alert(phrase.toUpperCase());
+
+
+
+
+
+var multipleValues=[];
+multipleValues[0]=50;
+multipleValues[1]=90;
+
+multipleValues[2]="Hello";
+alert(multipleValues[2]);
+
+
+var multipleValues=[50,90,"Hello"];
+
+
+var headline=document.getElementById("mainHeading")
+headline.innerHML="WOW,a new headline!"
+
+
+
